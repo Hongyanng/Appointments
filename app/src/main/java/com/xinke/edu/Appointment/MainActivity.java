a9e66323aac43e48d049c8ee3700ea48ff3d417b
帅哥 <xiang088645@gamil.com>
2023-11-05 22:12:08 +0800

    我改变了布局的样式

--- app/src/main/java/com/xinke/edu/Appointment/MainActivity.java
+++ app/src/main/java/com/xinke/edu/Appointment/MainActivity.java
@@ -2,7 +2,10 @@ package com.xinke.edu.Appointment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 import com.guit.edu.R;
 
@@ -10,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_login);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
     }
 }
