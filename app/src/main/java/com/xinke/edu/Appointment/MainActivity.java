163975b0e1a605580a0c1da98d277966f99003af
Hongyanng <1843600489@qq.com>
2023-11-05 13:34:36 +0800

    initial commit

---
+++ app/src/main/java/com/xinke/edu/Appointment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xinke.edu.Appointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.guit.edu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
