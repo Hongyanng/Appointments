11c2e9f81a75564c06dcb3bd753571cfbe3d0e20
Hongyanng <1843600489@qq.com>
2023-11-05 21:16:00 +0800

    1.设置默认启动界面为login.xml 2.新增注册界面

---
+++ app/src/main/java/com/xinke/edu/Appointment/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.xinke.edu.Appointment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
